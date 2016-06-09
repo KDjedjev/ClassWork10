e4d6df9e2cde599e0f6027e81234400725fb7bfc
kokodzh <kdjedjev@gmail.com>
2016-06-09 21:31:48 +0300

    Done

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+	Scanner in = new Scanner(System.in);
+	System.out.print("Vuvedete izrechenie");
+	String sentence = in.nextLine();
+	if (sentence.startsWith("Greeting")){
+		System.out.println("yes");
+	}
+	if(sentence.endsWith("."));
+	System.out.println("no");
+	if(sentence.contains("water"));{
+		System.out.print("yes");
+
+	}
+	}
+
+}
